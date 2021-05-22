3329228acb698fe529c8b40462e3c537c0e640a9
braddinh1952000@gmail.com <Congtai456@>
2021-05-22 18:20:29 +0700

    view profile

--- app/src/main/java/com/project/instagram/Fragment/ProfileFragment.java
+++ app/src/main/java/com/project/instagram/Fragment/ProfileFragment.java
@@ -6,6 +6,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -14,6 +15,8 @@ import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,12 +29,32 @@ import com.project.instagram.StartActivity;
 
 
 public class ProfileFragment extends Fragment {
-    ImageView ic_logout;
+    ImageView ic_logout, image_profile;
+    TextView post, following, followers, fullname, bio, username;
+    Button edit_profile;
+
+    FirebaseUser firebaseUser;
+    String profileid;
+
+    ImageButton my_fotos, my_fotos_saved;
+
     FirebaseAuth auth = FirebaseAuth.getInstance();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        SharedPreferences preferences = getContext().getSharedPreferences("PREFS", Context.MODE_PRIVATE);
+        profileid = preferences.getString("profileid", "none");
+        image_profile = view.findViewById(R.id.image_profile);
+        post = view.findViewById(R.id.post);
+        following = view.findViewById(R.id.following);
+        followers = view.findViewById(R.id.followers);
+        fullname = view.findViewById(R.id.fullname);
+        bio = view.findViewById(R.id.bio);
+        username = view.findViewById(R.id.username);
+        edit_profile = view.findViewById(R.id.edit_profile);
+
         ic_logout = view.findViewById(R.id.ic_logout);
         ic_logout.setOnClickListener(new View.OnClickListener() {
             @Override
